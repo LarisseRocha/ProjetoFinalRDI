8af28c84afff48dd489d156f8c6b017d03f5aeaf
LarisseRocha <larisse16rocha@gmail.com>
2022-07-04 17:06:47 -0300

    Alteracao na connection string

--- ProjetoFinalRDI/Context/ProjetoFinalContext.cs
+++ ProjetoFinalRDI/Context/ProjetoFinalContext.cs
@@ -6,9 +6,27 @@ namespace ProjetoFinalRDI.Context
     public class ProjetoFinalContext: DbContext
     {
 
-        public ProjetoFinalContext(DbContextOptions<ProjetoFinalContext> options) : base(options)
+            public ProjetoFinalContext(DbContextOptions<ProjetoFinalContext> options) : base(options)
             { 
+                
             }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+            {
+                if (!optionBuilder.IsConfigured)
+                {
+
+                    IConfigurationRoot configuration = new ConfigurationBuilder()
+                        .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                        .AddJsonFile("appsettings.json")
+                        .Build();
+
+                optionBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+                }
+
+            }
+
             public virtual DbSet<Aluno> Alunos { get; set; }
             public virtual DbSet<Turma> Turmas { get; set; }
             protected override void OnModelCreating(ModelBuilder modelBuilder)
